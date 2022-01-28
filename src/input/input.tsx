b333baf84dd38db50b017e6ea1a715231faf0c1f
Artemis <artemis@townland.xyz>
2022-01-28 14:37:04 +0330

    input class and style removed from props

--- src/input/input.tsx
+++ src/input/input.tsx
@@ -2,18 +2,23 @@ import React from "react"
 
 export const Input = (props: InputProps) => {
     const _input = () => {
+        const _props = Object.assign({}, props)
+
+        delete _props.className
+        delete _props.style
+
         return (
             <input
                 value={props.value}
                 type={props.type || 'text'}
                 placeholder={props.placeholder}
-                {...props}
+                {..._props}
             />
         )
     }
 
     return (
-        <div className="tl-input-container">
+        <div className={`tl-input-container ${props.className || ''}`}>
             {
                 props.prefix ? <div className="tl-input-prefix"><props.prefix /></div> : <></>
             }
